5fb25e26f6b39599fe16c1f0c009e48167205dad
zackluckyf <fanning.34@gmail.com>
2016-09-18 22:51:46 -0400

    problem 6 fully tested, could use refactored

--- problems/problem6.js
+++ problems/problem6.js
@@ -1,3 +1,5 @@
+// Write a function to find the rectangular intersection of two given love rectangles.
+
 var OkEros = (function() {
     'use strict';
 
@@ -11,15 +13,18 @@ var OkEros = (function() {
         for (var prop1 in topBoundaryAndHeight) {
             obj[prop1] = topBoundaryAndHeight[prop1];
         }
+        if (obj.width === 0 || obj.height === 0) {
+            obj = {};
+        }
         return obj;
     }
 
     function leftObject(loverObj1, loverObj2) {
         var obj = {};
-        var obj1left = loverObj1.leftX + loverObj1.width > loverObj2.leftX && loverObj1.leftX < loverObj2.leftX;
-        var obj2left = loverObj2.leftX + loverObj2.width > loverObj1.leftX && loverObj2.leftX < loverObj1.leftX;
-        var obj1right = loverObj1.leftX + loverObj1.width > loverObj2.leftX + loverObj2.width;
-        var obj2right = loverObj2.leftX + loverObj2.width > loverObj1.leftX + loverObj1.width;
+        var obj1left = loverObj1.leftX + loverObj1.width >= loverObj2.leftX && loverObj1.leftX <= loverObj2.leftX;
+        var obj2left = loverObj2.leftX + loverObj2.width >= loverObj1.leftX && loverObj2.leftX <= loverObj1.leftX;
+        var obj1right = loverObj1.leftX + loverObj1.width >= loverObj2.leftX + loverObj2.width;
+        var obj2right = loverObj2.leftX + loverObj2.width >= loverObj1.leftX + loverObj1.width;
         if (obj1left && obj1right) {
             obj = {
                 left: loverObj2.leftX,
@@ -46,10 +51,10 @@ var OkEros = (function() {
 
     function topObject(loverObj1, loverObj2) {
         var obj;
-        var obj1above = loverObj1.bottomY + loverObj1.height > loverObj2.bottomY + loverObj2.height && loverObj1.bottomY < loverObj2.bottomY + loverObj2.height;
-        var obj1below = loverObj1.bottomY < loverObj2.bottomY;
-        var obj2above = loverObj2.bottomY + loverObj2.height > loverObj1.bottomY + loverObj1.height && loverObj2.bottomY < loverObj1.bottomY + loverObj1.height;
-        var obj2below = loverObj2.bottomY < loverObj1.bottomY;
+        var obj1above = loverObj1.bottomY + loverObj1.height >= loverObj2.bottomY + loverObj2.height && loverObj1.bottomY <= loverObj2.bottomY + loverObj2.height;
+        var obj1below = loverObj1.bottomY <= loverObj2.bottomY;
+        var obj2above = loverObj2.bottomY + loverObj2.height >= loverObj1.bottomY + loverObj1.height && loverObj2.bottomY <= loverObj1.bottomY + loverObj1.height;
+        var obj2below = loverObj2.bottomY <= loverObj1.bottomY;
         if (obj1above && obj1below) {
             obj = {
                 top: loverObj2.bottomY + loverObj2.height,
--- spec/problem6.spec.js
+++ spec/problem6.spec.js
@@ -1,5 +1,6 @@
 describe("Problem6", function() {
-    var intersection, obj1, obj2;
+    'use strict';
+    var intersection, obj1, obj2, obj3, obj4, obj5, obj6, obj7, obj8, obj9, obj10, intersection2, intersection3;
     beforeEach(function() {
         obj1 = {
             leftX: 1,
@@ -25,6 +26,42 @@ describe("Problem6", function() {
             width: 10,
             height: 6
         };
+        obj5 = {
+            leftX: 1,
+            bottomY: 1,
+            width: 1,
+            height: 1
+        };
+        obj6 = {
+            leftX: 3,
+            bottomY: 3,
+            width: 1,
+            height: 1
+        };
+        obj7 = {
+            leftX: 1,
+            bottomY: 1,
+            width: 1,
+            height: 1
+        };
+        obj8 = {
+            leftX: 0,
+            bottomY: 0,
+            width: 2,
+            height: 2
+        };
+        obj9 = {
+            leftX: 1,
+            bottomY: 0,
+            width: 1,
+            height: 1
+        };
+        obj10 = {
+            leftX: 0,
+            bottomY: 0,
+            width: 1,
+            height: 1
+        };
         intersection = {
             left: 3,
             width: 8,
@@ -36,6 +73,12 @@ describe("Problem6", function() {
             width: 5,
             top: 11,
             height: 4
+        };
+        intersection3 = {
+            left: 1,
+            width: 1,
+            top: 2,
+            height: 1
         };
     });
     it("should have an intersect method", function() {
@@ -58,5 +101,14 @@ describe("Problem6", function() {
     });
     it("should return an object from intersect method second time", function() {
         expect(OkEros.intersect(obj3, obj4)).toEqual(intersection2);
+    });
+    it("should return an empty object if the objects dont intersect", function() {
+        expect(OkEros.intersect(obj5, obj6)).toEqual({});
+    });
+    it("should return the contained object if one object contains the other", function() {
+        expect(OkEros.intersect(obj7, obj8)).toEqual(intersection3);
+    });
+    it("should return an empty object if the objects touch but don't intersect", function() {
+        expect(OkEros.intersect(obj9, obj10)).toEqual({});
     });
 });
