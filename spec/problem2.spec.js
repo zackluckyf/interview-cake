d194b1b5439c3bd5db5361732f0fc411c994f506
zackluckyf <fanning.34@gmail.com>
2016-09-19 18:27:14 -0400

    added two tests for problem 2

--- spec/problem2.spec.js
+++ spec/problem2.spec.js
@@ -3,4 +3,12 @@ describe("Problem2", function() {
         var array = [1, 7, 3, 4];
         expect(values.productSum(array)).toEqual([84, 12, 28, 21]);
     });
+    it("should make a product array using the other array values round 2", function() {
+        var array = [2, 3, 9, 4];
+        expect(values.productSum(array)).toEqual([108, 72, 24, 54]);
+    });
+    it("should make a product array using the other array values round 3", function() {
+        var array = [3, 6, 7, 2];
+        expect(values.productSum(array)).toEqual([84, 42, 36, 126]);
+    });
 });
